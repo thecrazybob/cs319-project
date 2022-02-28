2d005bc88a38f688f835f0572a5f5b7a8bd5c50b
Mohammed Sohail <hey@msohail.dev>
2022-02-28 13:19:37 +0300

    Add filament tables

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
             colors: {
                 danger: colors.rose,
                 primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
             },
         },
     },
