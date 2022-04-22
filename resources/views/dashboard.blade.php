0aa420e772c9cd42bcd6f38b93da121e536ebc1b
DjTer3a <till.3OOV@gmail.com>
2022-04-22 22:02:12 +0300

    added correct route link to View my documents button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -167,7 +167,7 @@
                       </div>
                       <div class="mt-8">
                         <h3 class="text-lg font-medium">
-                          <a href="#" class="focus:outline-none">
+                          <a href="{{ route('patient.documents') }}" class="focus:outline-none">
                             <!-- Extend touch target to entire panel -->
                             <span class="absolute inset-0" aria-hidden="true"></span>
                             View my documents
