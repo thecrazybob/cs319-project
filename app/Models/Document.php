7b68343eb33a3fa0bcc74b57740a10064a8ea83d
DjTer3a <till.3OOV@gmail.com>
2022-05-07 17:04:31 +0300

    created document model and migration

---
+++ app/Models/Document.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+    'name',
+    'user_id',
+    'file_path'
+    ];
+    
+}
