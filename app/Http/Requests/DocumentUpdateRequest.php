ae8cb83b87c5cc0ebf1a40d393233dcb41f96038
Mohammed Sohail <hey@msohail.dev>
2022-05-08 17:34:13 +0300

    Add Validation Classes

---
+++ app/Http/Requests/DocumentUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DocumentUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'patient_id' => ['required', 'integer', 'exists:patients,id'],
+            'file_id' => ['required', 'integer', 'exists:files,id'],
+            'name' => ['required', 'string'],
+            'type' => ['required', 'string'],
+            'upload_date' => ['required', 'date'],
+        ];
+    }
+}
---
+++ app/Http/Requests/VaccineUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VaccineUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'patient_id' => ['required', 'integer', 'exists:patients,id'],
+            'vaccine_type' => ['required', 'in:covid,other'],
+            'vaccine_date' => ['required', 'date'],
+            'dose_no' => ['required', 'integer'],
+        ];
+    }
+}
