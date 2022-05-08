a4547b3d5dfb5fd6ee634ef41160f9770e2109f6
cutlope <abdullah77@outlook.com>
2022-05-09 00:38:14 +0300

    added patient resource

--- app/Nova/Patient.php
+++ app/Nova/Patient.php
@@ -2,25 +2,29 @@
 
 namespace App\Nova;
 
-use Illuminate\Http\Request;
 use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Patient extends Resource
 {
+    public function title()
+    {
+        return \App\Models\User::where('patient_id', $this->id)->first()->name;;
+    }
+
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
     public static $model = \App\Models\Patient::class;
-
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
-    public static $title = 'id';
 
     /**
      * The columns that should be searched.
@@ -41,6 +45,18 @@ class Patient extends Resource
     {
         return [
             ID::make()->sortable(),
+            Text::make('Bilkent Id')->sortable(),
+            Date::make('Birth Date')->sortable(),
+            Select::make('Gender')->options(['female' => 'Female', 'male' => 'Male', 'other' => 'Other']),
+            Number::make('Height')->min(50)->max(300)->step(0.1)->sortable(),
+            Number::make('Weight')->min(1)->max(1000)->step(0.1)->sortable(),
+            Textarea::make('Allergies'),
+            Textarea::make('Other Illnesses'),
+            Textarea::make('Current Medications'),
+            Boolean::make('Smoking'),
+            Date::make('Created At')->sortable(),
+            Date::make('Updated At')->sortable(),
+
         ];
     }
 
