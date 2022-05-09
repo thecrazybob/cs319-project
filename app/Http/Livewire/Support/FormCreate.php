3c9b3f57e0ccf691d8952c649dc7e2bf2c432105
DjTer3a <till.3OOV@gmail.com>
2022-05-10 01:22:10 +0300

    created create ticket

--- app/Http/Livewire/Support/FormCreate.php
+++ app/Http/Livewire/Support/FormCreate.php
@@ -4,8 +4,10 @@ namespace App\Http\Livewire\Support;
 
 use App\Models\Support;
 use Livewire\Component;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\BelongsToSelect;
 use Filament\Forms\Concerns\InteractsWithForms;
 
@@ -14,26 +16,41 @@ class FormCreate extends Component implements HasForms
     use InteractsWithForms;
 
     public $subject;
+    public $status;
+    public $priority;
+    public $department_id;
 
     protected function getFormSchema(): array
     {
         return [
-            Textarea::make('subject')
-                ->required(),
+                TextInput::make('subject')
+                ->required()
+                ->label('Subject:'),
             BelongsToSelect::make('department_id')
                 ->relationship('department', 'name'),
+            Select::make('priority')->options([
+                "low" => 'Low',
+                'medium' => 'Medium',
+                'high' => 'High',
+                'critical' => 'Critical',
+            ])
+            ->required()
+            ->label('Priority:'), 
         ];
     }
 
     public function submit(): void
     {
-        $array = array_merge(['user_id' => auth()->user()->id], $this->form->getState());
+        $array = array_merge([
+            'status' => 'new',
+            'patient_id' => auth()->user()->patient->id,
+            ],$this->form->getState());
 
-        $this->support->supportmessages()->create($array);
+            $support = Support::create($array);
 
-        toast()->success('Successfully replied to the support ticket')->push();
+        toast()->success('Successfully created a support ticket')->push();
 
-        redirect(route('support.show', $this->support));
+        redirect(route('support.show', $support));
     }
 
     protected function getFormModel(): string
