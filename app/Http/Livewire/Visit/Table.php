96ba41976a3c04129ef0276947203d79ae491a84
DjTer3a <till.3OOV@gmail.com>
2022-05-09 20:03:37 +0300

    making visits livewire components

---
+++ app/Http/Livewire/Visit/Table.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Visit;
+
+use Livewire\Component;
+
+class Table extends Component
+{
+    public function render()
+    {
+        return view('livewire.visit.table');
+    }
+}
