19ee719a27635c77721768c948a4bb2e4af6e49e
Mohammed Sohail <hey@msohail.dev>
2022-05-09 21:17:02 +0300

    Change create view

--- resources/views/support/create.blade.php
+++ resources/views/support/create.blade.php
@@ -3,10 +3,17 @@
         <div class="flex justify-between items-center">
             <div>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Tests') }}
+            {{ __('My Support Queries') }}
         </h2>
-        <p class="mt-2 text-sm text-gray-700">A list of all your tests in the health center.</p>
+        <p class="mt-2 text-sm text-gray-700">A list of all the support queries you have created so far.</p>
             </div>
+        <div>
+            <a href="{{ route('support.create') }}">
+                <x-jet-button wire:loading.attr="disabled" wire:target="photo">
+                    {{ __('Create a ticket') }}
+                </x-jet-button>
+            </a>
+        </div>
     </div>
     </x-slot>
 
@@ -17,7 +24,7 @@
                 <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                         <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                            <livewire:test.table />
+                            <livewire:support.table />
                         </div>
                     </div>
                 </div>
