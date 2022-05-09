7f8bbbc4a7a94a500b1ca32af92a85ca5e215f41
DjTer3a <till.3OOV@gmail.com>
2022-05-09 20:42:08 +0300

    updated and added livewire components for visits

--- app/Http/Livewire/Visit/Table.php
+++ app/Http/Livewire/Visit/Table.php
@@ -2,10 +2,43 @@
 
 namespace App\Http\Livewire\Visit;
 
+use App\Models\Visit;
 use Livewire\Component;
+use Illuminate\Contracts\View\View;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\IconButtonAction;
+use Filament\Tables\Concerns\InteractsWithTable;
 
-class Table extends Component
+class Table extends Component implements HasTable
 {
+    use InteractsWithTable;
+
+    protected function getTableQuery(): Builder
+    {
+        return Visit::where('patient_id', auth()->user()->patient->id);
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('id'),
+            TextColumn::make('visit_date')->date(),
+            TextColumn::make('created_at')->date(),
+            TextColumn::make('updated_at')->date(),
+        ];
+    }
+
+    protected function getTableEmptyStateIcon(): ?string
+    {
+        return 'heroicon-o-bookmark';
+    }
+
+    protected function getTableEmptyStateHeading(): ?string
+    {
+        return 'No visits yet';
+    }
     public function render()
     {
         return view('livewire.visit.table');
