5c060803491a33d28352fff29def24596064b020
faruk.ulutas <faruk.ulutas@ug.bilkent.edu.tr>
2022-05-10 04:38:39 +0300

    new-pay commitment

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -13,7 +13,7 @@ class InvoiceController extends Controller
      */
     public function index(Request $request)
     {
-        $invoices = Invoice::where('patient_id', $patient_id)->get();
+        $invoices = Invoice::where('patient_id', auth()->user()->patient_id)->get();
 
         return view('invoice.index', compact('invoices'));
     }
@@ -26,5 +26,14 @@ class InvoiceController extends Controller
     public function show(Request $request, Invoice $invoice)
     {
         return view('invoice.show', compact('invoice'));
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request, Invoice $invoice)
+    {
+        return view('invoice.pay', compact('invoice'));
     }
 }
--- app/Nova/Invoice.php
+++ app/Nova/Invoice.php
@@ -3,11 +3,8 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\Badge;
 use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\Currency;
 use Laravel\Nova\Fields\Date;
-use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Select;
@@ -49,26 +46,18 @@ class Invoice extends Resource
     {
         return [
             ID::make()->sortable(),
-            BelongsTo::make('Patient')->sortable(),
-            Currency::make('Amount')->min(0)->max(100000)->step(0.01)->sortable()->required()->currency('TRY'),
-            Badge::make('Status')->map([
-                "unpaid" => "danger",
-                "paid" => "success",
-                "cancelled" => "warning",
-                "refunded" => "info",
-                "partial" => "warning",
-            ])->sortable(),
+            BelongsTo::make('Patient')->sortable()->searchable(),
+            Number::make('Amount')->min(0)->max(100000)->step(0.01)->sortable(),
             Select::make('Status')->options([
                 'unpaid' => 'Unpaid',
                 'paid' => 'Paid',
                 'cancelled' => 'Cancelled',
                 'refunded' => 'Refunded',
                 'partial' => 'Partial',
-            ])->sortable()->required()->onlyOnForms(),
-            Text::make('Description')->required(),
-            Date::make('Created At')->sortable()->onlyOnDetail(),
-            Date::make('Updated At')->sortable()->onlyOnDetail(),
-            HasMany::make('Transactions'),
+            ])->sortable(),
+            Text::make('Description'),
+            Date::make('Created At')->sortable(),
+            Date::make('Updated At')->sortable(),
         ];
     }
 
